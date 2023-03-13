275793fc1f64d0097675a51eecfbaf446f8a52ee
zeddy5000 <zeedy5000@gmail.com>
2023-03-13 23:57:55 +0100

    updated collections

--- src/collections/ListTest.java
+++ src/collections/ListTest.java
@@ -1,36 +1,44 @@
 package collections;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class ListTest {
     public static void main(String[] args) {
         String [] colors = {"red","blue","green","purple","indigo","violet","black"};
         List<String> mainList = new LinkedList<>();
-        for(String color : colors){
-            mainList.add(color);
-        }
+        Collections.addAll(mainList, colors);
         String [] newColors = {"magenta","cyan","deep blue"};
         List<String> secList = new LinkedList<>();
-        for(String color : newColors){
-            secList.add(color);
-        }
+        Collections.addAll(secList, newColors);
         mainList.addAll(secList);
-        secList = null;
         System.out.println("This is the appended list" + mainList);
 
 
         System.out.println("The uppercase modified list is " + convertToUpperCase(mainList) );
+        removeItems(mainList,4,7);
+        System.out.println("The modified list contains elements: "+ mainList);
+        printReverseList(mainList);
     }
 
     private static List<String> convertToUpperCase(List<String> list) {
         Iterator<String> iterator = list.listIterator();
         List<String> newList = new LinkedList<>();
         while (iterator.hasNext()){
-            String color = iterator.next().toUpperCase();
+            String color = iterator.next();
+            ((ListIterator<String>)iterator).set(color.toUpperCase());
             newList.add(color);
         }
         return newList;
+    }
+    private static void removeItems(List<String> list, int start, int end){
+        list.subList(start,end).clear();
+    }
+    private static void printReverseList(List<String> list){
+        ListIterator<String> iterator = list.listIterator(list.size());
+        System.out.println("The reverse list has elements: ");
+        while (iterator.hasPrevious()){
+            System.out.print(iterator.previous() + " ");
+        }
+
     }
 }
