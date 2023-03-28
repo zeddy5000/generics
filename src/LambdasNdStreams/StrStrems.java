5f2cde749e4d582bd8ecc0a9b170c6a69390791c
zeddy5000 <zeedy5000@gmail.com>
2023-03-28 18:17:21 +0100

    update

--- src/LambdasNdStreams/StrStrems.java
+++ src/LambdasNdStreams/StrStrems.java
@@ -1,7 +1,20 @@
 package LambdasNdStreams;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StrStrems {
     public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("zed");
+        names.add("helen");
+        names.add("sam");
 
+        List<String> capsNmanes = names.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+        System.out.println(capsNmanes);
     }
 }
