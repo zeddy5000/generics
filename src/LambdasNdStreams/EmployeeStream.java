c772cdf363abfb91f0c13fe5f84731fb7ca5f1b5
zeddy5000 <zeedy5000@gmail.com>
2023-04-03 23:01:59 +0100

    updated

--- src/LambdasNdStreams/EmployeeStream.java
+++ src/LambdasNdStreams/EmployeeStream.java
@@ -1,11 +1,9 @@
 package LambdasNdStreams;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class EmployeeStream {
 
@@ -14,7 +12,8 @@ public class EmployeeStream {
                 new Employee("Helen","Isu",300.0,"IT"),
                 new Employee("Sammy","Isu",400.0,"Engr"),
                 new Employee("Faith","Isu",200.0,"Medic"),
-                new Employee("Adam","Johnson",603.0,"Staff")
+                new Employee("Adam","Johnson",603.0,"Staff"),
+                new Employee("Mary","Oluchi",700,"Medic")
         };
         List<Employee> list = new ArrayList<>(Arrays.asList(employees));
         Predicate<Employee> twoAndAbove = employee -> (employee.getSalary() > 200 && employee.getSalary() <= 700);
@@ -25,5 +24,20 @@ public class EmployeeStream {
                 .filter(twoAndAbove)
                 .sorted(lastThenFirst.reversed())
                 .forEach(System.out::println);
+
+         list.stream()
+                .map(Employee::getDepartment)
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+        System.out.printf("%nEmployees by department: %n");
+        System.out.println();
+        Map<String,List<Employee>> groupByDept = list.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
+        groupByDept.forEach((department,employeesInDepartment)->{
+            System.out.printf("%n%S%n",department);
+            employeesInDepartment.forEach(employee -> System.out.printf("%s%n",employee));
+        });
+
     }
 }
