1bfcd792b58f1084b6c46f40ff33b736f5a46ace
zeddy5000 <zeedy5000@gmail.com>
2023-04-03 13:04:49 +0100

    updated

--- src/LambdasNdStreams/EmployeeStream.java
+++ src/LambdasNdStreams/EmployeeStream.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EmployeeStream {
@@ -12,13 +13,17 @@ public class EmployeeStream {
         Employee [] employees = {new Employee("Zeddy","Isu",200.0,"IT"),
                 new Employee("Helen","Isu",300.0,"IT"),
                 new Employee("Sammy","Isu",400.0,"Engr"),
-                new Employee("Faith","Isu",200.0,"Medic")
+                new Employee("Faith","Isu",200.0,"Medic"),
+                new Employee("Adam","Johnson",603.0,"Staff")
         };
         List<Employee> list = new ArrayList<>(Arrays.asList(employees));
-        Predicate<Employee> twoAndAbove = employee -> (employee.getSalary() > 200 && employee.getSalary() <= 400);
+        Predicate<Employee> twoAndAbove = employee -> (employee.getSalary() > 200 && employee.getSalary() <= 700);
+        Function<Employee,String> firstName = Employee::getFirstName;
+        Function<Employee,String> lastName = Employee::getLastName;
+        Comparator<Employee> lastThenFirst = Comparator.comparing(lastName).thenComparing(firstName);
         list.stream()
                 .filter(twoAndAbove)
-                .sorted(Comparator.comparing(Employee::getSalary))
+                .sorted(lastThenFirst.reversed())
                 .forEach(System.out::println);
     }
 }
